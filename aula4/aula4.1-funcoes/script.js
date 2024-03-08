358720e19c4c29e54d8a5cc774855754c55523a1
gustavo-goulart <gustavofgoulart@uol.com.br>
2024-03-08 13:19:53 -0300

    Incluindo aula 4.1

---
+++ aula4/aula4.1-funcoes/script.js
@@ -0,0 +1,11 @@
+// var output = document.querySelector("#output");
+
+// output.innerHTML = "Texto inserido através do JavaScript";
+
+let mensagem = "";
+
+for (i = 1; i <= 100; i++) {
+  mensagem += "i: " + i + "<br>";
+}
+
+output.innerHTML = mensagem;
